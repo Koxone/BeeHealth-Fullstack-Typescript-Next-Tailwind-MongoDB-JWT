f1202a2e081c947950e4206d545a04038785efb2
Carlos de Leon <admin@koxland.net>
2025-10-23 21:05:44 -0600

    Rename page component for new patient appointment
    Renamed the default export from 'page' to 'PatientNewAppointmentPage' in the new appointment page for patients to improve clarity and consistency.

--- src/app/patient/appointments/new/page.jsx
+++ src/app/patient/appointments/new/page.jsx
@@ -1,7 +1,7 @@
 import NewAppointment from '@/components/sections/Patient/Appointments/New/NewAppointment';
 import React from 'react';
 
-function page() {
+function PatientNewAppointmentPage() {
   return (
     <div className="h-screen overflow-hidden pb-40">
       <NewAppointment />
@@ -9,4 +9,4 @@ function page() {
   );
 }
 
-export default page; 
+export default PatientNewAppointmentPage;
